0cb0a572e22c5edbfdd0279fe551a8e09fb15355
trungkien1402 <ptkienzz1@gmail.com>
2025-12-07 14:56:08 +0700

    edit-trangchu

--- js/thong-ke.js
+++ js/thong-ke.js
@@ -1,17 +1,5 @@
 // =========================================================
-const canvas = document.getElementById("chart");
-const ctx = canvas.getContext("2d");
 
-ctx.strokeStyle = "#00ffd0";
-ctx.lineWidth = 3;
-
-ctx.beginPath();
-ctx.moveTo(20, 120);
-ctx.lineTo(80, 80);
-ctx.lineTo(140, 100);
-ctx.lineTo(200, 60);
-ctx.lineTo(260, 90);
-ctx.stroke();
 // =========================================================
 // --- 1. Dữ liệu Tự động Khởi tạo cho 150 Bàn ---
 
